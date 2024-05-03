42f90ccef57f26acb06553691f4eb6e09354b112
Dang Quoc Dat <dat03122003@gmail.com>
2024-05-03 23:28:38 +0700

    Updating Booking page and Passing Data from Form to PaymentController

---
+++ WebTourTravel/Controllers/PaymentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using WebTourTravel.Models;
+
+namespace WebTourTravel.Controllers
+{
+    public class PaymentController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index()
+        {
+            // Lấy dữ liệu từ form
+            string name = Request.Form["Name"];
+            string email = Request.Form["Email"];
+            string phoneNumber = Request.Form["PhoneNumber"];
+            string ghiChu = Request.Form["GhiChu"];
+            string paymentMethod = Request.Form["PhuongThucThanhToan"];
+            int slNguoiLon = int.Parse(Request.Form["SoLuongNguoiLon"]);
+            int slTreEm = int.Parse(Request.Form["SoLuongTreEm"]);
+
+            string idTour = Request.Form["IdTour"];
+
+            double tongTien = double.Parse(Request.Form["TongTien"]);
+
+
+            // Map dữ liệu vào model
+            ModelBooking model = new ModelBooking
+            {
+                Name = name,
+                Email = email,
+                PhoneNumber = phoneNumber,
+                GhiChu = ghiChu,
+                SoLuongNguoiLon = slNguoiLon,
+                SoLuongTreEm = slTreEm,
+                PhuongThucThanhToan = paymentMethod,
+                IdTour = idTour,
+                TongTien = tongTien
+            };
+
+            return View(model);
+        }
+
+    }
+}
---
+++ WebTourTravel/Models/ModelBooking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebTourTravel.Models
+{
+    public class ModelBooking
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string GhiChu { get; set; }
+        public string PhuongThucThanhToan { get; set; }
+
+        public string IdTour { get; set; }
+        public double TongTien { get; set; }
+
+        public int SoLuongNguoiLon { get; set; }
+        public int SoLuongTreEm { get; set; }
+
+    }
+}
