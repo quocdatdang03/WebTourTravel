327c62f8b4ae1b1dbedaf10485551da9e2c97cc4
Dang Quoc Dat <dat03122003@gmail.com>
2024-05-25 21:41:16 +0700

    UPDATE Profile and History

--- WebTourTravel/Controllers/ProfileController.cs
+++ WebTourTravel/Controllers/ProfileController.cs
@@ -3,15 +3,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebTourTravel.Models;
 
 namespace WebTourTravel.Controllers
 {
     public class ProfileController : Controller
     {
-        // GET: Profile
+        private TourDuLichEntities db = new TourDuLichEntities();
+
+        // GET: Profile/History
         public ActionResult History(string idUser)
-        { 
-            return View();
+        {
+            if (string.IsNullOrEmpty(idUser))
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest, "Invalid User ID");
+            }
+
+            try
+            {
+                var orderTours = GetOrderTours(idUser);
+                return View("History", orderTours);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                // Log.Error(ex);
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.InternalServerError, "An error occurred while fetching the tour history.");
+            }
+        }
+
+        // GET: Profile/ChoXacNhan
+        public ActionResult ChoXacNhan(string idUser, int idStatus)
+        {
+            if (string.IsNullOrEmpty(idUser))
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest, "Invalid User ID");
+            }
+
+            try
+            {
+                var orderTours = GetOrderTours(idUser, idStatus);
+                return View("ChoXacNhan", orderTours);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                // Log.Error(ex);
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.InternalServerError, "An error occurred while fetching the pending confirmations.");
+            }
+        }
+
+        // GET: Profile/DaDat
+        public ActionResult DaDat(string idUser, int idStatus)
+        {
+            if (string.IsNullOrEmpty(idUser))
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest, "Invalid User ID");
+            }
+
+            try
+            {
+                var orderTours = GetOrderTours(idUser, idStatus);
+                return View("DaDat", orderTours);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                // Log.Error(ex);
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.InternalServerError, "An error occurred while fetching the confirmed bookings.");
+            }
+        }
+
+        private List<ModelOrderTour> GetOrderTours(string idUser, int? idStatus = null)
+        {
+            var query = from hoadon in db.HoaDon
+                        join tour in db.Tour on hoadon.id_tour equals tour.id_tour
+                        where hoadon.Gmail.Equals(idUser)
+                        select new ModelOrderTour
+                        {
+                            IdTour = tour.id_tour,
+                            TenTour = tour.TenTour,
+                            KhoiHanh = tour.KhoiHanh,
+                            Avata = tour.Avata,
+                            SoLuongHanhKhach = hoadon.SoLuongHanhKhach,
+                            Gmail = hoadon.Gmail,
+                            TrangThaiDonHang = hoadon.TrangThaiDonHang
+                        };
+
+            if (idStatus.HasValue)
+            {
+                query = query.Where(hoadon => hoadon.TrangThaiDonHang == idStatus.Value);
+            }
+
+            return query.ToList();
         }
     }
 }
---
+++ WebTourTravel/Models/ModelOrderTour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebTourTravel.Models
+{
+    public class ModelOrderTour
+    {
+        public string IdTour { get; set; }
+        public string TenTour { get; set; }
+        public DateTime KhoiHanh { get; set; }
+        public string Avata { get; set; }
+        public Nullable<int> SoLuongHanhKhach { get; set; }
+        public string Gmail { get; set; }
+        public Nullable<int> TrangThaiDonHang { get; set; }
+    }
+}
